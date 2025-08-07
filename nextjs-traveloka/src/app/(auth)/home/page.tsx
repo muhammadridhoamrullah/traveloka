c5cf9d18612fa643ab2556af705951e5cbca78df
HP <ridhoamrullah99@gmail.com>
2025-08-07 15:18:39 +0700

    FE NextJS: Navbar Done, Home Tinggal Form Pemesanan

--- nextjs-traveloka/src/app/(auth)/home/page.tsx
+++ nextjs-traveloka/src/app/(auth)/home/page.tsx
@@ -1,8 +1,50 @@
 "use client";
 
 import { generateMetaData } from "@/db/utils/metadata";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Homepage() {
+  const airlineCompanies = [
+    "/forTrustedBy/airasia-logo.png",
+    "/forTrustedBy/batik-air-logo.png",
+    "/forTrustedBy/citilink-logo.png",
+    "/forTrustedBy/garuda-indonesia-logo.png",
+    "/forTrustedBy/lion-air-logo.png",
+    "/forTrustedBy/nam-air-logo.png",
+    "/forTrustedBy/pelita-air-logo.png",
+    "/forTrustedBy/sriwijaya-air-logo.png",
+    "/forTrustedBy/super-air-jet-logo.png",
+    "/forTrustedBy/transnusa-logo.png",
+    "/forTrustedBy/trigana-air-service-logo.png",
+    "/forTrustedBy/wings-air-logo.png",
+  ];
+
+  const GROUP_SIZE = 5;
+
+  // state current index for the airline companies
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // hitung jumlah grup yang bisa ditampilkan
+  const totalGroups = Math.ceil(airlineCompanies.length / GROUP_SIZE);
+  console.log("Total Groups:", totalGroups);
+
+  // dapatkan logo dari grup yang sedang aktif
+  const currentGroup = airlineCompanies.slice(
+    currentIndex * GROUP_SIZE,
+    currentIndex * GROUP_SIZE + GROUP_SIZE
+  );
+  console.log("Current Group:", currentGroup);
+
+  // set interval untuk mengubah tiap 2 detik
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % totalGroups);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [totalGroups]);
+
   // Generate metadata
 
   generateMetaData({
@@ -21,8 +63,32 @@ export default function Homepage() {
   });
 
   return (
-    <div className="pt-36 h-[800px] bg-[url('/bg_home.jpg')] bg-cover bg-center text-white">
-      <h1>Your homepage</h1>
+    <div className="w-full min-h-screen">
+      <div className="pt-36 pb-5 w-full min-h-screen bg-[url('/bg_home.jpg')] bg-cover bg-center text-white flex flex-col justify-between items-center">
+        <div className="font-bold text-3xl">
+          Plan, Book & Travel: Flights, Hotels & Activities Worldwide
+        </div>
+        <div>FORM PEMESANAN</div>
+        <div className="flex justify-center items-center flex-col gap-2 ">
+          <div className="text-sm">Trusted By</div>
+          <div className="flex justify-center items-center gap-6 flex-wrap transition-opacity duration-1000 p-2 border border-slate-700   rounded-md">
+            {currentGroup.map((logo, index) => (
+              <Image
+                key={index}
+                src={logo}
+                alt={`Airline logo ${index}`}
+                width={30}
+                height={22}
+                className="object-contain transition-opacity duration-1000 opacity-100 hover:scale-110"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div>Section 2</div>
+      <div>Section 3</div>
+      <div>Section 4</div>
+      <div>Section 5</div>
     </div>
   );
 }
--- nextjs-traveloka/src/app/components/Navbar.tsx
+++ nextjs-traveloka/src/app/components/Navbar.tsx
@@ -5,6 +5,16 @@ import { useEffect, useState } from "react";
 import { PiUserCircleLight } from "react-icons/pi";
 import { IoIosArrowUp } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
+import { FaRegUser } from "react-icons/fa";
+import { CiCreditCard1 } from "react-icons/ci";
+import { RiFileList3Line } from "react-icons/ri";
+import { GoChecklist } from "react-icons/go";
+import { RiRefund2Line } from "react-icons/ri";
+import { FaRegBell } from "react-icons/fa";
+import { MdPeopleOutline } from "react-icons/md";
+import { IoMailOutline } from "react-icons/io5";
+import { BiLogOut } from "react-icons/bi";
+import Image from "next/image";
 
 export default function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -27,25 +37,52 @@ export default function Navbar() {
 
   function whenScrolled() {
     return isScrolled
-      ? "bg-white shadow-lg text-[#0194F3]"
+      ? "bg-white shadow-lg text-black"
       : "bg-transparent text-white";
+  }
+
+  function styleListDropdown() {
+    return "px-4 py-3 hover:bg-black/40 flex items-center gap-4";
+  }
+
+  function styleListMenuNavbarBawah() {
+    return "p-2 rounded-md hover:bg-black/80";
   }
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full ${whenScrolled()} h-28 py-4 px-20  z-50 shadow-lg flex flex-col justify-between transition-all duration-1000`}
+      className={`fixed top-0 left-0 w-full ${whenScrolled()} h-28 gap-2 py-4 px-20  z-50 shadow-lg flex flex-col justify-between transition-all duration-1000`}
     >
-      <div className="h-1/2 flex justify-between items-center text-sm">
-        <div>Logo Traveloka</div>
+      {/* Awal Navbar Atas */}
+      <div className="h-1/2 flex justify-between items-center text-sm ">
+        {isScrolled ? (
+          <Image
+            src={"/traveloka_hitam.png"}
+            width={150}
+            height={150}
+            alt="Traveloka Logo"
+          />
+        ) : (
+          <Image
+            src={"/traveloka_putih.png"}
+            width={150}
+            height={200}
+            alt="Traveloka Logo"
+          />
+        )}
         <div className="flex gap-4 items-center ">
-          <div>IDR | ID</div>
+          <Link className="p-2 rounded-md hover:bg-black/40" href={"/idr-id"}>
+            IDR | ID
+          </Link>
           <Link
             className="p-2 rounded-md hover:bg-black/40"
             href={"/promotion"}
           >
             Deals
           </Link>
-          <div>Support</div>
+          <Link className="p-2 rounded-md hover:bg-black/40" href={"/support"}>
+            Support
+          </Link>
           <Link
             className="p-2 rounded-md hover:bg-black/40"
             href={"/partnetship"}
@@ -72,28 +109,87 @@ export default function Navbar() {
               <div className="absolute top-16 right-20 w-64 bg-blue-950 py-4 rounded-md shadow-lg  text-white">
                 <ul className="flex flex-col text-xs font-semibold">
                   <li className="font-bold text-xl px-4 mb-2">Nama Gweh</li>
-                  <li className=" px-4 py-3 hover:bg-black/40">Edit Profile</li>
-                  <li className=" px-4 py-3 hover:bg-black/40 ">My Cards</li>
-                  <li className=" px-4 py-3 hover:bg-black/40 ">
-                    Purchase List
-                  </li>
-                  <li className=" px-4 py-3 hover:bg-black/40 ">My Booking</li>
-                  <li className=" px-4 py-3 hover:bg-black/40 ">Refunds</li>
-                  <li className=" px-4 py-3 hover:bg-black/40 ">
-                    Flight Price Alerts
-                  </li>
-                  <li className=" px-4 py-3 hover:bg-black/40 ">
-                    Saved Passenger Details
-                  </li>
-                  <li className=" px-4 py-3 hover:bg-black/40 ">Promo Info</li>
-                  <li className=" px-4 py-3 hover:bg-black/40 ">Log Out</li>
+                  <Link href={"/profile"} className={styleListDropdown()}>
+                    <FaRegUser className="text-lg" />
+                    <div>Edit Profile</div>
+                  </Link>
+                  <Link href={"/my-cards"} className={styleListDropdown()}>
+                    <CiCreditCard1 className="text-lg" />
+                    <div>My Cards</div>{" "}
+                  </Link>
+                  <Link href={"/purchase-list"} className={styleListDropdown()}>
+                    <RiFileList3Line className="text-lg" />
+                    <div>Purchase List</div>
+                  </Link>
+                  <Link href={"/my-booking"} className={styleListDropdown()}>
+                    <GoChecklist className="text-lg" />
+                    <div>My Booking</div>
+                  </Link>
+                  <Link href={"refunds"} className={styleListDropdown()}>
+                    <RiRefund2Line className="text-lg" />
+                    <div>Refunds</div>
+                  </Link>
+                  <Link
+                    href={"flight-price-alerts"}
+                    className={styleListDropdown()}
+                  >
+                    <FaRegBell className="text-lg" />
+                    <div>Flight Price Alerts</div>
+                  </Link>
+                  <Link
+                    href={"saved-passenger-details"}
+                    className={styleListDropdown()}
+                  >
+                    <MdPeopleOutline className="text-lg" />
+                    <div>Saved Passenger Details</div>
+                  </Link>
+                  <Link href={"promo-info"} className={styleListDropdown()}>
+                    <IoMailOutline className="text-lg" />
+                    <div>Promo Info</div>
+                  </Link>
+                  <Link href={"logout"} className={styleListDropdown()}>
+                    <BiLogOut className="text-lg" />
+                    <div>Log Out</div>
+                  </Link>
                 </ul>
               </div>
             )}
           </div>
         </div>
       </div>
-      <div className=" h-1/2 flex justify-between items-center">Menu-menu</div>
+      {/* Akhir Navbar Atas */}
+
+      {/* Awal Navbar Bawah */}
+      <div className=" h-1/2 flex justify-between items-end text-sm ">
+        <div
+          className={`flex justify-start items-center gap-5 font-semibold ${
+            isScrolled ? "text-slate-400" : "text-white"
+          } `}
+        >
+          <Link className={styleListMenuNavbarBawah()} href={"/hotels"}>
+            Hotels
+          </Link>
+          <Link className={styleListMenuNavbarBawah()} href={"/flights"}>
+            Flights
+          </Link>
+          <Link
+            className={styleListMenuNavbarBawah()}
+            href={"/airport-transfer"}
+          >
+            Airport Transfer
+          </Link>
+          <Link className={styleListMenuNavbarBawah()} href={"/car-rental"}>
+            Car Rental
+          </Link>
+          <Link className={styleListMenuNavbarBawah()} href={"/thing-to-do"}>
+            Things To Do
+          </Link>
+          <Link className={styleListMenuNavbarBawah()} href={"/travel-guides"}>
+            Travel Guides
+          </Link>
+        </div>
+      </div>
+      {/* Akhir Navbar Bawah */}
     </div>
   );
 }
